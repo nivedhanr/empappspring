6c9403caa86ea9471cfec6ee13d7f449d609bdfe
nivedhanr <nivedharenahan@gmail.com>
2022-12-06 22:17:10 +0530

    view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddempComponent } from './addemp/addemp.component';
+import { ViewempComponent } from './viewemp/viewemp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddempComponent
+    AddempComponent,
+    ViewempComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/viewemp/viewemp.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-viewemp',
+  templateUrl: './viewemp.component.html',
+  styleUrls: ['./viewemp.component.css']
+})
+export class ViewempComponent {
+data:any=[]
+}
